d79bd78e4289e0609aa760fbd40c175a6624e5ce
ZEXX TUNNELING <ariksaputra081205@gmail.com>
2025-04-07 23:35:58 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
-# Getting
+
 clear
-IP=$(wget -qO- ipinfo.io/ip);
+IP=$(curl -s ipinfo.io/ip)
 date=$(date +"%Y-%m-%d")
-clear
 email=$(cat /home/email)
-if [[ "$email" = "" ]]; then
-echo "Masukkan Email Untuk Menerima Backup"
-read -rp "Email : " -e email
-cat <<EOF>>/home/email
-$email
-EOF
-fi
-clear
-figlet "Backup"
-echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
-rm -rf /root/backup
-mkdir /root/backup
-cp /etc/passwd backup/
-cp /etc/group backup/
-cp /etc/shadow backup/
-cp /etc/gshadow backup/
-cp -r /etc/xray backup/xray
-cp -r /root/nsdomain backup/nsdomain
-cp -r /etc/slowdns backup/slowdns
-cp -r /home/vps/public_html backup/public_html
+backup_dir="/root/backup"
+zip_name="${IP}-${date}.zip"
+
+# Siapkan folder backup
+rm -rf "$backup_dir"
+mkdir -p "$backup_dir"
+
+# proses backup
+cp /etc/passwd "$backup_dir/"
+cp /etc/group "$backup_dir/"
+cp /etc/shadow "$backup_dir/"
+cp /etc/gshadow "$backup_dir/"
+cp -r /etc/xray "$backup_dir/xray"
+cp -r /root/nsdomain "$backup_dir/nsdomain"
+cp -r /etc/slowdns "$backup_dir/slowdns"
+cp -r /home/vps/public_html "$backup_dir/public_html"
+
+# Buat ZIP
 cd /root
-zip -r $IP-$date.zip backup > /dev/null 2>&1
-rclone copy /root/$IP-$date.zip zexx:TUNNELING/
-url=$(rclone link zexx:TUNNELING/$IP-$date.zip)
-id=(`echo $url | grep '^https' | cut -d'=' -f2`)
+zip -r "$zip_name" backup > /dev/null 2>&1
+
+# Upload ke rclone (folder TUNNELING)
+rclone copy "/root/$zip_name" "zexx:TUNNELING/"
+url=$(rclone link "zexx:TUNNELING/$zip_name")
+id=$(echo "$url" | cut -d'=' -f2)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-echo -e "
-Detail Backup 
+
+# Kirim email
+message=$(cat <<EOF
+Detail Backup
 ==================================
 IP VPS        : $IP
 Link Backup   : $link
 Tanggal       : $date
 ==================================
-" | mail -s "Backup Data" $email
-rm -rf /root/backup
-rm -r /root/$IP-$date.zip
+EOF
+)
+
+echo "$message" | mail -s "Backup Data" "$email"
+
+# Bersihkan
+rm -rf "$backup_dir" "/root/$zip_name"
+
+# Tampilkan ke terminal
 clear
-echo -e "
-Detail Backup 
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-"
-echo "Silahkan cek Kotak Masuk $email"
+echo "$message"
+echo "Silakan cek kotak masuk email: $email"
