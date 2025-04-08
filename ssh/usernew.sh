c824f8f2de9c569a75eaa9cc31a3f6c189007525
ZEXX TUNNELING <ariksaputra081205@gmail.com>
2025-04-08 08:17:54 +0700

    Update usernew.sh

--- ssh/usernew.sh
+++ ssh/usernew.sh
@@ -43,7 +43,7 @@ read -p "Password       : " Pass
 read -p "Limit IP       : " limitip
 read -p "Expired (Days) : " expired
 useradd -e `date -d "$expired days" +"%Y-%m-%d"` -s /bin/false -M $Login
-exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+exp="$(chage -l $Login | grep "Account expired" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 hari-ini=`date -d "0 days" +"%Y-%m-%d"`
 echo "${limitip}" > /etc/xray/limit/ssh/ip/${user}
